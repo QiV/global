7cf899ab03bfd212d2b6735bb1ce39ab6a76f089
Kuznetsov Ilia <i.kuznecov@corp.mail.ru>
2015-07-31 17:59:22 +0300

    +navigator

--- window.js
+++ window.js
@@ -1,9 +1,26 @@
+var mock = {
+  location: {
+    ancestorOrigins: {},
+    origin: "",
+    hash: "",
+    search: "",
+    pathname: "",
+    port: "",
+    hostname: "",
+    host: "",
+    protocol: "",
+    href: ""
+  }
+};
+
 if (typeof window !== "undefined") {
     module.exports = window;
 } else if (typeof global !== "undefined") {
-    module.exports = global;
+    module.exports = Object.assign(global, mock);
 } else if (typeof self !== "undefined"){
     module.exports = self;
 } else {
-    module.exports = {};
+    module.exports = mock
 }
+
+
